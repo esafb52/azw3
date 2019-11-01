995e2189dca45627492fafb2d79f9fce51de138b
masiha <esafb52@gmail.com>
2019-11-01 02:54:50 -0700

    add setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='azw3',
+    version='1.1',
+    packages=['azw3'],
+    url='',
+    license='GPL',
+    author='masiha ahmadi',
+    author_email='esafb52@gmail.com',
+    description='basic utility tools using calibre app for convert docx to azw3 format for kindle'
+    ,
+    long_description=open('README.txt').read(),
+)
